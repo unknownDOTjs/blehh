c7d35756205173e9aca99bf12e39e3e524dd8283
TimC21171 <158390128+TimC21171@users.noreply.github.com>
2025-10-30 10:54:50 +1300

    another teeny weeny intsy beeny ass lame as commit thats like two lines big

--- project/public/scripts/createAccount.js
+++ project/public/scripts/createAccount.js
@@ -8,26 +8,22 @@ const password2Error = document.getElementById("password2Error");
 const emailError = document.getElementById("emailError");
 const submitButton = document.getElementById("submitButton");
 const verificationCodeError = document.getElementById("verificationCodeError");
+const resendCode = document.getElementById("resendCode");
 
 form.addEventListener("submit", function(event) {
     event.preventDefault();
 
-    const formData = new FormData(form);
+    sendFormData();
 
-    const data = Object.fromEntries(formData); 
-    if (data.password1 !== data.password2){ password2Error.innerHTML = "Passwords are not the same" }
-    else{ 
-        password2Error.innerHTML = "";
-        submitButton.style.filter = "brightness(60%)";
-        submitButton.style.pointerEvents = "none";
-        socket.emit("checkUserData", data.username, data.password2, data.email)
-    }
 });
 
 socket.on("invalidUserData", (errors)=>{
 
     submitButton.style.filter = "brightness(100%)";
     submitButton.style.pointerEvents = "auto";
+
+    resendCode.style.filter = "brightness(100%)";
+    resendCode.style.pointerEvents = "auto";
 
     if (!errors[0][0]){
 
@@ -64,6 +60,9 @@ socket.on("validUserData", ()=>{
     form.style.display = "none";
     document.getElementById("accountVerificationContainer").style.display = "block";
 
+    resendCode.style.filter = "brightness(100%)";
+    resendCode.style.pointerEvents = "auto";
+
 
 })
 
@@ -99,5 +98,26 @@ function verifyAccount(){
     codeButton.style.pointerEvents = "none";
 
     socket.emit("verifyAccount", inputCode.value)
+
+}
+
+function sendFormData(){
+
+    const formData = new FormData(form);
+
+    const data = Object.fromEntries(formData); 
+    if (data.password1 !== data.password2){ password2Error.innerHTML = "Passwords are not the same" }
+    else{ 
+        password2Error.innerHTML = "";
+        submitButton.style.filter = "brightness(60%)";
+
+        resendCode.style.filter = "brightness(60%)";
+        resendCode.style.pointerEvents = "auto";
+
+        submitButton.style.pointerEvents = "none";
+        resendCode.style.pointerEvents = "none";
+
+        socket.emit("checkUserData", data.username, data.password2, data.email)
+    }
 
 }
